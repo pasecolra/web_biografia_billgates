8bb29a3fcfdcc12e458c86ccb5165efa6a8cfe9b
pasecolra <paulcolmenares@colegioguanenta.edu.co>
2024-07-09 08:49:29 -0500

    falta css

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Event listeners para los botones del menú
+document.getElementById("btnInicio").addEventListener("click", function() {
+    alert("Has clickeado en Inicio");
+  });
+  
+  document.getElementById("btnProductos").addEventListener("click", function() {
+    alert("Has clickeado en Productos");
+  });
+  
+  document.getElementById("btnServicios").addEventListener("click", function() {
+    alert("Has clickeado en Servicios");
+  });
+  
+  document.getElementById("btnContacto").addEventListener("click", function() {
+    alert("Has clickeado en Contacto");
+  });
+  
